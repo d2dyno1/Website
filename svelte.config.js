d3d85075bb76acfec24f632fd252407d3d894168
TheOnlyTails <theonlytails@theonlytails.com>
2021-07-01 21:08:42 +0300

    Prepared for Heroku deployment.

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,8 @@
 import preprocess from "svelte-preprocess";
 import autoprefixer from "autoprefixer";
+import adapter from "@sveltejs/adapter-node";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
@@ -16,6 +17,15 @@ const config = {
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: adapter({
+			// default options are shown
+			out: "build",
+			precompress: false,
+			env: {
+				host: "0.0.0.0",
+				port: "PORT",
+			},
+		}),
 		target: "#svelte",
 	},
 };
